c246b3454a04bada756e64eb3b48266a443acfaf
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2019-01-06 15:39:10 +0100

    add modules compile

--- make_kernel.sh
+++ make_kernel.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+corenum=$(grep -c ^processor /proc/cpuinfo)
+echo "Running make with: " $corenum " threads"
+
 cp -r linux_zturn/* linux-xlnx/
-cp vivado_prj/vivado_prj/vivado_prj.sdk/device_tree_bsp_0/pcw.dtsi linux-xlnx/arch/arm/boot/dts/
-cp vivado_prj/vivado_prj/vivado_prj.sdk/device_tree_bsp_0/zynq-7000.dtsi linux-xlnx/arch/arm/boot/dts/
-cp vivado_prj/vivado_prj/vivado_prj.sdk/device_tree_bsp_0/pl.dtsi linux-xlnx/arch/arm/boot/dts/
-cp vivado_prj/vivado_prj/vivado_prj.sdk/device_tree_bsp_0/system-top.dts linux-xlnx/arch/arm/boot/dts/zynq-zturn.dts
+cp vivado_prj/hdmi_prj/hdmi_prj.sdk/device_tree_bsp_0/pcw.dtsi linux-xlnx/arch/arm/boot/dts/
+cp vivado_prj/hdmi_prj/hdmi_prj.sdk/device_tree_bsp_0/zynq-7000.dtsi linux-xlnx/arch/arm/boot/dts/
+cp vivado_prj/hdmi_prj/hdmi_prj.sdk/device_tree_bsp_0/pl.dtsi linux-xlnx/arch/arm/boot/dts/
+cp vivado_prj/hdmi_prj/hdmi_prj.sdk/device_tree_bsp_0/system-top.dts linux-xlnx/arch/arm/boot/dts/zynq-zturn.dts
 cd linux-xlnx/
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- zynq_z_turn_sbf_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage LOADADDR=0x00008000 -j12
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage LOADADDR=0x00008000 -j$corenum
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- dtbs
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules_install INSTALL_MOD_PATH=../modules
 cp arch/arm/boot/uImage ../BOOT
 cp arch/arm/boot/dts/zynq-zturn.dtb ../BOOT/devicetree.dtb
